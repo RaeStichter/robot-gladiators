deb7326de9d9ea492866852177eca3c10e1d839c
Rae Stichter <raestichter@gmail.com>
2020-10-29 00:43:02 -0400

    add enemy combatants

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,22 +1,37 @@
+// Game States:
+// "WIN" - Player ronot has defeated all enemy-robots
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+// Global variables:
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this:
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+/*
+console.log(enemyNames);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+} */
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
     // Ask the playwe whether they want to fight or skip
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    console.log(promptFight);
+    //console.log(promptFight);
 
     // if player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -66,8 +81,10 @@ var fight = function() {
     else {
         window.alert("You need to choose a valid option.  Try again!");
     }
-    console.log(playerName, playerAttack, playerHealth, playerMoney);
+    //console.log(playerName, playerAttack, playerHealth, playerMoney);
 
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
